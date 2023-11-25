4090867c151660afbbf3fa228e93179f0bb5cf0e
Murod0404 <muroddadaboev07@gmail.com>
2023-11-25 15:47:01 +0500

    commit new 3

--- Test.java
+++ Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    // niam dur nima dur  lullas  yandi commit
+    /** ffsfsfSasdasfhrhtjtyjytgfdg sfffffffffffdfdsfsfff*/
+    public void inss(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+ "<<");
+        }
+    }
 }
